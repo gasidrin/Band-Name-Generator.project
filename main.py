5a6e9acaba9b8bd79e273f9bcd173a17d02c9921
gasidrin <88278590+gasidrin@users.noreply.github.com>
2023-01-06 02:56:06 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to Band Name Generator!\n \n In this program you will be able to get a name band based on your natal city and the name of your first pet.\n")
+
+city = input("What's the name of your natal city?\n")
+pet = input("What's the name of your first pet?\n")
+
+
+
+print("Your band name could be " + city + " " + pet + ".")
